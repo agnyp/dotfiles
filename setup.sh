2d7ae314a8e50b8e20a0522a94040d56d26c0525
Andreas Gnyp <andreas@gnyp.de>
2019-04-07 23:59:06 +0200

    Setup file links pearl.conf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo Installing pearl...
+PEARL_CONFIG=~/.config/pearl/pearl.conf
+PEARL_CONFIG_DIR=~/.config/pearl
 
+echos(){
+  echo
+  echo [linux-setup] $1
+  echo
+}
+echos "Installing pearl..."
 wget -O install-pearl.sh https://raw.githubusercontent.com/pearl-core/installer/master/install.sh
 bash install-pearl.sh
+
+echos "linking pearl.conf..."
+mv $PEARL_CONFIG $PEARL_CONFIG.bk
+ln -s `pwd`/pearl.conf $PEARL_CONFIG_DIR
